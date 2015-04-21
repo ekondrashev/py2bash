02630776a3f948e4ee1533fb25b36631ed8bde70
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2015-04-21 23:42:59 +0300

    Added a test for single boundary

--- test/test_for_loop.py
+++ test/test_for_loop.py
@@ -10,7 +10,7 @@ from py2bash import py2bash
 class Test(unittest.TestCase):
 
 
-    def test_for_loop_seq(self):
+    def test_for_loop_seq_both_boundaries(self):
         actual = py2bash('''for i in range(0, 5):
     print i''')
         expected = '''for i in {0..4}
@@ -18,7 +18,15 @@ do
     echo $i
 done'''
         self.assertEquals(expected, actual)
-
+    
+    def test_for_loop_seq_end_only(self):
+        actual = py2bash('''for i in range(5):
+    print i''')
+        expected = '''for i in {0..4}
+do
+    echo $i
+done'''
+        self.assertEquals(expected, actual)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
